6a6880ae1fdfc356145c5730518320e56dae5fe3
MichielsenM <mathias.michielsen@hotmail.com>
2021-05-27 15:35:39 +0200

    Read both ascii and hdf MESA files consistently

--- pypulse/functions_for_mesa.py
+++ pypulse/functions_for_mesa.py
@@ -1,9 +1,9 @@
 """A few helpful functions to process MESA output."""
-# from PyPulse import functions_for_mesa as ffm
+# from pypulse import functions_for_mesa as ffm
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import multiprocessing, glob, csv
+import multiprocessing, glob, csv, h5py
 from pathlib import Path
 from functools import partial
 from pypulse import my_python_functions as mypy
@@ -11,51 +11,82 @@ from pypulse import my_python_functions as mypy
 ################################################################################
 def read_mesa_file(file_path):
     """
-    Read in a mesa profile or history file, and return 2 pandas dataframes.
+    Read in a mesa profile or history file and return 2 dictionaries.
     The first with the header info, and the second with the data.
+    If the format is hdf5, assumes the attributes are the MESA header.
+    If the format is not an hdf5 file, assumes the default MESA output format.
+    This format is an ascii file delimited by whitespace with the following structure:
+    line 1: header names
+    line 2: header data
+    line 3: blank
+    line 4: main data names
+    line >4:main data values
+
     ------- Parameters -------
     file_path: String
         The path to the MESA profile or history file to read in.
     ------- Returns -------
-    header: pandas dataframe
-        A dataframe holding the header info of the MESA file.
-    data: pandas dataframe
-        A dataframe holding the data of the MESA file, the column used for indexing is
-        the 'zone' for profiles, and the 'model_number' for the history file.
+    header: dictionary
+        A dictionary holding the header info of the MESA file.
+    data: dictionary
+        A dictionary holding the data columns of the MESA file as numpy arrays.
     """
-    header = pd.read_table(file_path, delim_whitespace=True, nrows=1, header=1)
-    data = pd.read_table(file_path, delim_whitespace=True, skiprows = 3, header=1, index_col=0)
 
-    return header, data
+    if h5py.is_hdf5(file_path):
+        with h5py.File(file_path, 'r') as file:
+            # Read attributes (MESA header)
+            header = dict(zip(file.attrs.keys(),file.attrs.values()))
+            # Read datasets (MESA main data)
+            data={}
+            for k in file.keys() :
+                data[k] = file[k][...]
+
+        return header, data
+
+    else:   # assumes the default MESA output format
+        header_df = pd.read_table(file_path, delim_whitespace=True, nrows=1, header=1)
+        data_df = pd.read_table(file_path, delim_whitespace=True, skiprows = 3, header=1, index_col=0)
+
+        header={}
+        for k in header_df.keys():
+            header.update({k: header_df[k].to_numpy()[0]})
+        data = {}
+        for k in data_df.keys():
+            data.update({k: data_df[k].to_numpy()})
+
+        return header, data
 
 ################################################################################
 def check_hydro_eq(profile_file, treshold_for_plot=5E-8):
     """
-    Makes a plot showing the normalised differences between the terms on both sides of the hydrostatic equilibrium equation.
-    This shows how well hydrostatic equilibrium has been fulfilled in your MESA model.
+    Calculates the normalised differences between the terms on both sides of the hydrostatic equilibrium equation.
+    Makes a plot if the values exceed a given treshold.
+    This shows how well hydrostatic equilibrium has been fulfilled in the MESA model.
     ------- Parameters -------
     profile_file: String
-        The path to the profile file to be used for the plot.
+        The path to the profile file to be checked.
     treshold_for_plot: float
         Make a plot if the max difference between the left and right hand side of the equation is greater than this number.
     """
     header, data = read_mesa_file(profile_file)
     # Compute the hydrostatic equilibrium quality factor q - See e.g. Aerts et al. (2010)
-    norm = np.max(np.vstack([data['hyeq_lhs'], data['hyeq_rhs']]), axis=0)
-    hyeq = np.abs(data['hyeq_lhs'] - data['hyeq_rhs']) / np.abs(norm)
-    hyeq_NaNs_removed = [h for h in hyeq if str(h) != 'nan']
+    lhs=np.delete(data['hyeq_lhs'], 0)  # remove the values at the surface, since these are 0
+    rhs=np.delete(data['hyeq_rhs'], 0)
+
+    norm = np.max(np.vstack([lhs, rhs]), axis=0)
+    hyeq = np.abs(lhs - rhs) / np.abs(norm)
 
     # Make the plot if the treshold criterion is met
-    if max(hyeq_NaNs_removed) > treshold_for_plot:
+    if max(hyeq) > treshold_for_plot:
         # print the maximal deviation and profile name
-        print(max(hyeq_NaNs_removed))
+        print(max(hyeq))
         print(profile_file)
         # make a semilog plot
         fig=plt.figure()
         ax = fig.add_subplot(111)
-        ax.semilogy(data['radius'], hyeq, 'ko-')
+        ax.semilogy(np.delete(data['radius'], 0), hyeq, 'ko-') #also remove surface value, to have same amount of datapoints
         plt.show()
-    plt.close('all')
+        plt.close('all')
 
 ################################################################################
 def plot_profile(profile_file, x_value, y_value, ax=None, label_size=16, colour='', linestyle='solid', alpha=1, legend=True, label=None):
@@ -88,6 +119,7 @@ def plot_profile(profile_file, x_value, y_value, ax=None, label_size=16, colour=
         label = y_value
     if x_value == 'radius' or x_value == 'mass':
         x = x/x[0] # normalized radius/mass coordinates
+        ax.set_xlim(0,1)
     # generate the plot, in which colour will not be specified
     if colour == '':
         ax.plot( x , y, label=label, linestyle=linestyle, alpha=alpha)
@@ -125,7 +157,7 @@ def mesh_histogram(profile_file, x_value='radius', ax=None, label_size=16, colou
         ax = fig.add_subplot(111)
 
     header, data = read_mesa_file(profile_file)
-    print(f'Total zones of {profile_file} : {header["num_zones"][0]}')
+    print(f'Total zones of {profile_file} : {header["num_zones"]}')
 
     # from "data", extract the columns
     x   = np.asarray(data[x_value])
@@ -133,6 +165,7 @@ def mesh_histogram(profile_file, x_value='radius', ax=None, label_size=16, colou
         legend = False
     if x_value == 'radius' or x_value == 'mass':
         x = x/x[0] # normalized radius/mass coordinates
+        ax.set_xlim(0,1)
     # generate the plot, in which colour will not be specified
     if colour == '':
         ax.hist(x, bins=bins, histtype=u'step', label=label, alpha=alpha, linestyle=linestyle)
@@ -200,20 +233,19 @@ def calculate_number_densities(hist_file):
     '''
     Calculate surface number densities for all isotopes in the MESA grid.
     All isotopes in the used nuclear network need to be be written in the history file,
-    or this will function give wrong numbers.
+    otherwise this will function give wrong numbers.
     ------- Parameters -------
     hist_file: String
-        The path to the profile file to be used for the plot.
+        The path to the MESA history file.
     ------- Returns -------
-    number_densities: pandas dataframe
-        Column keys specify the element (surf_X_per_N_tot), row keys the model number
-        Values are number densities of that element
+    number_densities: dictionary
+        Column keys specify the element (surf_X_per_N_tot), values are number densities of that element.
     '''
     header, data = read_mesa_file(hist_file)
     element_list = {}
     number_densities = {}
-    inverse_average_atomic_mass = np.zeros(data.shape[0])
-    for column_name in data.columns:
+    inverse_average_atomic_mass = np.zeros(len(data[ list(data.keys())[0] ]))
+    for column_name in data.keys():
         if '_per_Mass_tot' in column_name:
             element_list.update({column_name: data[column_name]})
             inverse_average_atomic_mass += data[column_name]
@@ -259,7 +291,7 @@ def grid_extract_spectroscopy(mesa_profiles, output_file='gridSpectroscopy.tsv',
     mesa_profiles: string
         String to glob to find all the relevant MESA profiles.
     output_file: string
-        Name (can include a path) for the file containing all the pulsation frequencies of the grid.
+        Name (or path) for the file containing all the pulsation frequencies of the grid.
     parameters: list of strings
         List of parameters varied in the computed grid, so these are taken from the
         name of the profile files, and included in the ouput file containing the info of the whole grid.
@@ -297,10 +329,10 @@ def spectro_from_profiles(mesa_profile, parameters):
         Line containing all the model- and spectroscopic parameters of the MESA profile.
     """
     param_dict = mypy.get_param_from_filename(mesa_profile, parameters)
-    prof_data = mypy.read_hdf5(mesa_profile)
+    prof_header, prof_data = read_mesa_file(mesa_profile)
 
-    logL = np.log10(float(prof_data['photosphere_L']))
-    logTeff = np.log10(float(prof_data['Teff']))
+    logL = np.log10(float(prof_header['photosphere_L']))
+    logTeff = np.log10(float(prof_header['Teff']))
     logg = prof_data['log_g'][0]
 
     line=[]
