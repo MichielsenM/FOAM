eb2c2fd7a10fc2df71c3cd8f5d97cc57b448a994
MichielsenM <mathias.michielsen@hotmail.com>
2022-03-04 17:56:41 +0100

    Commented non-default options in config file

--- foam/pipeline/example_config.py
+++ foam/pipeline/example_config.py
@@ -5,19 +5,19 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%
 logger = logging.getLogger('logger')
 logger.setLevel(logging.DEBUG)
 
-star = 'KIC7760680'                             # Name of the star
-observations = 'data_KIC7760680.tsv'            # File with all the observational data
+star = #'KIC7760680'                             # Name of the star
+observations = #'data_KIC7760680.tsv'            # File with all the observational data
 periods_or_frequencies_observed = 'period'      # # Use the observed periods or frequencies, be consistent in observable_list later
 # Pulsation with the highest amplitude to build pattern from in 'highest_amplitude' method. Array with highest amplitude per part of the split pattern
-highest_amplitude_pulsation = {'period': [1.158919], 'frequency': [None]} # ordered the same as the file with the observations
+highest_amplitude_pulsation = {'period': [None], 'frequency': [None]} # ordered the same as the file with the observations
 
-gyre_dir = '/lhome/mathiasm/Software/gyre-6.0.1' # GYRE directory
-kval = 0                  # the mode ID of your g-mode pattern
-mval = 1
-rotation_gyre = '0.6304_prograde'
+gyre_dir = #'/lhome/mathiasm/Software/gyre-6.0.1' # GYRE directory
+kval = #0                  # the mode ID of your g-mode pattern
+mval = #1
+rotation_gyre = # '0.6304_prograde'
 
 # Parent directory of the computed grid, and names of the directories of the different grids
-grid_parent_directory = '/lhome/mathiasm/MESA_grid_15140'
+grid_parent_directory = #'/lhome/mathiasm/MESA_grid_15140'
 grids = ['ECP', 'DO']
 
 # Methods to construct the theoretical frequency pattern that are to be used
@@ -27,7 +27,7 @@ pattern_methods = ['chisq_longest_sequence','highest_amplitude', 'highest_freque
 merit_functions = ['chi2', 'mahalanobis']
 
 # Lists of observables to fit
-# observable_list = [['period'], ['period_spacing'], ['period', 'period_spacing', 'rope_length', 'logTeff', 'logL', 'logg'], ['period', 'period_spacing']]
+# observable_list = [['period'], ['period_spacing'], ['period', 'logTeff', 'logL', 'logg'], ['frequency']]
 observable_list = [['period'], ['period_spacing']]
 
 # calculate AIC for these observables (abbreviated names)
@@ -38,7 +38,7 @@ subgrid = '*'
 fixed_parameters = None # {'aov': 0.0, 'fov': 0.0}
 free_parameters=['M', 'Z', 'logD', 'aov', 'fov', 'Xc']
 k = len(free_parameters) # Number of free parameters
-N_periods = 36      # Number of periods
+N_periods = #36      # Number of periods
 N_pattern_parts = 1
 # Number of observables, calculated from the number of periods
 # Number of period spacings is number of periods minus amount of separated patterns.
