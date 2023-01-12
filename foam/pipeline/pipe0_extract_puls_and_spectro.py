501626cc4565a6f684a750ad88bd2efa08e446ee
MichielsenM <mathias.michielsen@hotmail.com>
2023-01-12 13:42:08 +0100

    Rotation_gyre as float, get rotation spin sense based on k-value

--- foam/pipeline/pipe0_extract_puls_and_spectro.py
+++ foam/pipeline/pipe0_extract_puls_and_spectro.py
@@ -3,9 +3,17 @@ from foam import functions_for_gyre as ffg
 from foam import functions_for_mesa as ffm
 import config # imports the config file relative to the location of the main script
 ################################################################################
+
+if config.mval>0:
+    rotation_spin_direction = 'prograde'
+elif config.mval==0:
+    rotation_spin_direction = 'zonal'
+elif config.mval<0:
+    rotation_spin_direction = 'retrograde'
+
 for grid in config.grids:
     output_file = f'../grid_summary/spectroGrid_{grid}.tsv'
     ffm.grid_extract_spectroscopy(f'{config.grid_parent_directory}/{grid}/MESA_out/Zini*/profiles/*{config.subgrid}*prof', output_file=output_file)
 
-    output_file = f'../grid_summary/pulsationGrid_{grid}_{config.rotation_gyre}.tsv'
-    ffg.extract_frequency_grid(f'{config.grid_parent_directory}/{grid}/GYRE_out/rot{config.rotation_gyre}/Zini*/*{config.subgrid}*.HDF', output_file=output_file)
+    output_file = f'../grid_summary/pulsationGrid_{grid}_{config.rotation_gyre}_{rotation_spin_direction}.tsv'
+    ffg.extract_frequency_grid(f'{config.grid_parent_directory}/{grid}/GYRE_out/rot{config.rotation_gyre}_{rotation_spin_direction}/Zini*/*{config.subgrid}*.HDF', output_file=output_file)
--- foam/pipeline/pipe1_construct_pattern.py
+++ foam/pipeline/pipe1_construct_pattern.py
@@ -9,7 +9,13 @@ import config # imports the config file relative to the location of the main scr
 
 # The required asymptotic class object
 asymp_obj = grs.asymptotic(gyre_dir=config.gyre_dir, kval=config.kval, mval=config.mval)
-estimated_rotation = float(config.rotation_gyre.split('_')[0])
+
+if config.mval>0:
+    rotation_spin_direction = 'prograde'
+elif config.mval==0:
+    rotation_spin_direction = 'zonal'
+elif config.mval<0:
+    rotation_spin_direction = 'retrograde'
 
 # Construct the pulsation patterns according to the different methods for the extracted theoretical grids
 observable = config.periods_or_frequencies_observed
@@ -17,7 +23,7 @@ for grid in config.grids:
     spectro= f'../grid_summary/spectroGrid_{grid}.tsv'
     for method in config.pattern_methods:   # Methods to construct theoretical pulsation patterns
         puls_file = f'extracted_freqs/{observable}_{config.star}_{grid}_{method}.tsv'
-        bop.construct_theoretical_freq_pattern(f'../grid_summary/pulsationGrid_{grid}_{config.rotation_gyre}.tsv', config.observations, method, highest_amplitude_pulsation=config.highest_amplitude_pulsation[observable], which_observable=observable, output_file=puls_file, asymptotic_object=asymp_obj, estimated_rotation=estimated_rotation)
+        bop.construct_theoretical_freq_pattern(f'../grid_summary/pulsationGrid_{grid}_{config.rotation_gyre}_{rotation_spin_direction}.tsv', config.observations, method, highest_amplitude_pulsation=config.highest_amplitude_pulsation[observable], which_observable=observable, output_file=puls_file, asymptotic_object=asymp_obj, estimated_rotation=config.rotation_gyre)
 
         # Merge spectro and pulsation info into one file
         output_name = f'{Path(puls_file).parent}/spectro+{Path(puls_file).name}'
