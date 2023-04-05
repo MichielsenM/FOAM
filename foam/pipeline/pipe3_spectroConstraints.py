2a7b1f056e0fd43e89b22cdbac3da1d2d120668b
MichielsenM <mathias.michielsen@hotmail.com>
2023-04-05 17:24:36 +0200

    Remove timing

--- foam/pipeline/pipe3_spectroConstraints.py
+++ foam/pipeline/pipe3_spectroConstraints.py
@@ -17,17 +17,10 @@ if config.n_sigma_spectrobox != None:
     params.remove('Xc')
     summary = mg.gridSummary(params)
 
-    import time
-    t1=time.perf_counter()
     if not Path('isocloud_grid.h5').is_file():
         summary.create_summary_file(config.isocloud_grid_directory, columns=['star_age','log_L','log_Teff','log_g'], magnitudes=False, output_name='isocloud_grid.h5', file_ending='hist', files_directory_name='history')
-        t2=time.perf_counter()
-        print(f'Creating isocloud took: {t2-t1}')
     else:
         summary.read_summary_file('isocloud_grid.h5')
-
-        t2=time.perf_counter()
-        print(f'Reading isocloud took: {t2-t1}')
 
     isocloud_summary_dict = {}
     for Z in summary.Z_array:
@@ -43,9 +36,8 @@ if config.n_sigma_spectrobox != None:
                         df_MZ = pd.concat([df_MZ, df], ignore_index=True)
             isocloud_summary_dict[Z].update({M : df_MZ })
 
-    t3=time.perf_counter()
-    print(f'Making dicts took: {t3-t2}')
 
+    all_files_kept = []
     for grid in config.grids:
         files = glob.glob(f'extracted_freqs/{config.star}_{grid}*.hdf')
         files_kept = list(files)
@@ -54,22 +46,9 @@ if config.n_sigma_spectrobox != None:
             if Path(output_file).is_file():
                 files_kept.remove(file)
                 config.logger.warning(f'file already existed: {output_file}')
+        all_files_kept.extend(files_kept)
 
-        with multiprocessing.Pool(1) as p:
-            func = partial( ac.spectro_constraint,  observations_file=observations, nsigma=config.n_sigma_spectrobox, spectroGrid_file=f'{config.main_directory}/../grid_summary/spectroGrid_{grid}.hdf',
-                                spectro_companion=config.spectro_companion, isocloud_grid_summary=isocloud_summary_dict)
-            # for result in p.map(func, files_kept):
-                # item=result
-            # p.map_async(func, files_kept)
-            p.map(func, files_kept)
-            p.close()
-            p.join()
-
-
-        # for file in files:
-        #     output_file = f'{config.n_sigma_spectrobox}sigmaSpectro_{file}'
-        #     if not (Path(output_file).is_file()):
-        #         ac.spectro_constraint(file, observations_file=observations, nsigma=config.n_sigma_spectrobox, spectroGrid_file=f'{config.main_directory}/../grid_summary/spectroGrid_{grid}.hdf',
-        #                             spectro_companion=config.spectro_companion, isocloud_grid_summary=isocloud_summary_dict)
-        #     else:
-        #         config.logger.warning(f'file already existed: {output_file}')
+    with multiprocessing.Pool(4) as p: # For some reason 4 is fastest, find out why more becomes slower
+        func = partial( ac.spectro_constraint,  observations_file=observations, nsigma=config.n_sigma_spectrobox, spectroGrid_file=f'{config.main_directory}/../grid_summary/spectroGrid_{grid}.hdf',
+                            spectro_companion=config.spectro_companion, isocloud_grid_summary=isocloud_summary_dict)
+        p.map(func, all_files_kept)
