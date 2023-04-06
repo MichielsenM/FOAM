60ea66faf0027402aed98d498eee2868d9dd7f5c
MichielsenM <mathias.michielsen@hotmail.com>
2023-04-06 15:28:56 +0200

    move make_multipanel_plot() to plot_tools

--- foam/support_functions.py
+++ foam/support_functions.py
@@ -4,8 +4,6 @@ import h5py, re
 import pandas as pd
 from pathlib import Path
 import logging
-import matplotlib.pyplot as plt
-from matplotlib.gridspec import GridSpec
 
 logger = logging.getLogger('logger.sf')
 ################################################################################
@@ -105,67 +103,6 @@ def read_hdf5(filename):
         for k in file.keys() :
             data[k] = file[k][...]
     return attributes, data
-################################################################################
-def make_multipanel_plot(nr_panels=1, xlabel='', ylabels=[''], keys=None, title='', label_size=22, xlim=[],
-                        left_space=0.1, bottom_space=0.085, right_space=0.978, top_space=0.97, h_space=0.12, figure_size = [12,8]):
-    """
-    Make a plot
-
-    ------- Parameters -------
-    nr_panels: int
-        The number of panels to add to the plot
-    xlabel, ylabels: string and list of strings
-        Names for x and y axes.
-    keys:
-        The keys corresponding to the dictionary entries of the axes
-    title: string
-        Title of the plot, no title if argument not given.
-    left_space, bottom_space, right_space, top_space: float, optional
-        The space that needs to be left open around the figure.
-    h_space: float
-        The size of the space in between both panels.
-    figure_size: list of 2 floats
-        Specify the dimensions of the figure in inch.
-    label_size: float
-        The size of the labels on the axes, and slightly smaller on the ticks
-    xlim: list
-        Lists of length 2, specifying the lower and upper limits of the x axe.
-        Matplotlib sets the limits automatically if the arguments are not given.
-
-    ------- Returns -------
-    ax_dict: Dictionary of the axes
-    fig: The figure
-    """
-    if keys==None:   # make keys integers
-        keys = range(nr_panels)
-
-    fig=plt.figure(figsize=(figure_size[0], figure_size[1]))
-    gs=GridSpec(nr_panels,1) # multiple rows, 1 column
-    ax_dict = {}
-
-    for i in range(0, nr_panels):
-        if i==0:
-            ax = fig.add_subplot(gs[i:i+1,0])
-            if len(xlim) ==2:
-                ax.set_xlim(xlim[0],xlim[1])
-
-        else:
-            ax = fig.add_subplot(gs[i:i+1,0], sharex=ax_dict[keys[0]])
-
-        ax_dict.update({keys[i]:ax})
-
-        ax.set_ylabel(ylabels[i], size=label_size)
-        ax.tick_params(labelsize=label_size-2)
-
-        if i == nr_panels-1:
-            ax.set_xlabel(xlabel, size=label_size)
-        else:
-            plt.setp(ax.get_xticklabels(), visible=False)
-
-    ax_dict[keys[0]].set_title(title)
-    plt.subplots_adjust(hspace=h_space, left=left_space, right=right_space, top=top_space, bottom=bottom_space)
-
-    return ax_dict, fig
 
 ################################################################################
 def sign(x):
